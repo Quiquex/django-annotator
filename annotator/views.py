9115a2dd76e772c3f7232c6f76896aca54640856
Quiquex <Quiquex@users.noreply.github.com>
2017-08-06 14:43:56 +0200

    Updated to use django's JsonResponse (1.7+)

--- annotator/views.py
+++ annotator/views.py
@@ -1,37 +1,17 @@
+import json
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import (HttpResponse,
                          HttpResponseForbidden,
                          HttpResponseBadRequest)
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 
 import annotator
 from annotator import models, serializers
-
-
-class JSONResponse(HttpResponse):
-    """
-    An ``HttpResponse`` that renders its content into JSON.
-    """
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs["content_type"] = "application/json"
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-def root(request):
-    if request.method == "GET":
-        return JSONResponse({"name": getattr(settings,
-                                             "ANNOTATOR_NAME",
-                                             "django-annotator-store"),
-                             "version": annotator.__version__})
-    else:
-        return HttpResponseForbidden()
 
 
 @csrf_exempt
@@ -39,9 +19,9 @@ def index_create(request):
     if request.method == "GET":
         annotations = models.Annotation.objects.all()
         serializer = serializers.AnnotationSerializer(annotations, many=True)
-        return JSONResponse(serializer.data)
+        return JsonResponse(serializer.data)
     elif request.method == "POST":
-        data = JSONParser().parse(request)
+        data = json.loads(request.body)
         serializer = serializers.AnnotationSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -60,10 +40,10 @@ def read_update_delete(request, pk):
     if request.method == "GET":
         annotation = get_object_or_404(models.Annotation, pk=pk)
         serializer = serializers.AnnotationSerializer(annotation)
-        return JSONResponse(serializer.data, status=200)
+        return JsonResponse(serializer.data, status=200)
     elif request.method == "PUT":
         annotation = get_object_or_404(models.Annotation, pk=pk)
-        data = JSONParser().parse(request)
+        data = json.loads(request.body)
         serializer = serializers.AnnotationSerializer(annotation, data=data)
         if serializer.is_valid():
             serializer.save()
@@ -86,7 +66,7 @@ def search(request):
         query = {k: v for k, v in request.GET.items()}
         annotations = models.Annotation.objects.filter(**query)
         serializer = serializers.AnnotationSerializer(annotations, many=True)
-        return JSONResponse({"total": len(serializer.data), "rows": serializer.data})
+        return JsonResponse({"total": len(serializer.data), "rows": serializer.data})
     else:
         return HttpResponseForbidden()
 
